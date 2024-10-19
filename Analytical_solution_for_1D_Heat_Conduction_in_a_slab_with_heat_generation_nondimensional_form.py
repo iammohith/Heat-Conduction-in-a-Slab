04b16901f52ba106cd28592f196223af6f97974c
Mohith Sai Gorla <46181469+iammohith@users.noreply.github.com>
2024-10-19 15:20:27 +0530

    Update Analytical_solution_for_1D_Heat_Conduction_in_a_slab_with_heat_generation_nondimensional_form.py

--- Analytical_solution_for_1D_Heat_Conduction_in_a_slab_with_heat_generation_nondimensional_form.py
+++ Analytical_solution_for_1D_Heat_Conduction_in_a_slab_with_heat_generation_nondimensional_form.py
@@ -4,30 +4,57 @@ Created on Sun Apr 12 18:09:49 2020
 
 @author: Mohith Sai
 """
-"""Lets us consider a slab with thickness of L with heat generation Qg
-and T1 & T2 be the temperatures of the both ends respectively adn they are equal to T1
-K thermal conductivity of the material
-the equation is T(x) = 4*(zeta - zeta**2)*(Tmax - T1) + T1
-where Tmax = T1 + Qg*L**2/8*k zeta = x/L"""
+
+# This program calculates the temperature distribution across a slab of thickness L, 
+# with heat generation (Qg). The temperatures at both ends of the slab are equal (T1).
+# The temperature distribution is given by the equation:
+# T(x) = 4 * (zeta - zeta**2) * (Tmax - T1) + T1
+# Where Tmax = T1 + (Qg * L**2) / (8 * K) and zeta = x / L
 
 import numpy as np
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 
-T1 = 120 #temperature at both ends are same  units must be degC
-L = 2 #the length of the slab units must be in meters
-Qg = 10 #heat generation units must be in W/m**3
-K = 1000 #thermal conductivity units must be in W/m*k
-n = 20 #the number of points
-x = np.linspace(0, L, n) #linearlyspaced points
-Tmax =  T1 + (Qg*L**2)/(8*K)
-T = np.ones(n) #preallocating array of ones for temperature
-zeta = np.ones(n) #preallocating array of ones for zeta
-for i in range(0, n) :
-    zeta[i] = x[i]/L
-    T[i] = (4*(zeta[i] - zeta[i]**2))*(Tmax - T1) + T1 #temperature distribution
+# Defining the temperature at both ends of the slab (in degrees Celsius)
+T1 = 120  # Temperature at both ends (degC)
+
+# Defining the thickness of the slab (in meters)
+L = 2  # Length of the slab (m)
+
+# Heat generation rate (in W/m^3)
+Qg = 10  # Heat generation per unit volume (W/m^3)
+
+# Thermal conductivity of the slab material (in W/m*K)
+K = 1000  # Thermal conductivity (W/m*K)
+
+# Number of points along the slab to compute the temperature
+n = 20  # Number of points
+
+# Generate n linearly spaced points between 0 and L
+x = np.linspace(0, L, n)
+
+# Calculate Tmax (maximum temperature at the center of the slab)
+Tmax = T1 + (Qg * L**2) / (8 * K)
+
+# Preallocate arrays for temperature (T) and zeta
+T = np.ones(n)  # Preallocate temperature array
+zeta = np.ones(n)  # Preallocate zeta array (zeta = x / L)
+
+# Calculate the temperature distribution across the slab
+for i in range(n):
+    zeta[i] = x[i] / L  # Calculate zeta for each point
+    T[i] = (4 * (zeta[i] - zeta[i]**2)) * (Tmax - T1) + T1  # Temperature distribution
+
+# Plotting the temperature distribution
 plt.figure(1)
-plt.plot(x,T,color='red', linestyle='dashed', marker='.', markerfacecolor='blue')
-plt.xlabel('Distacnce (m)')
-plt.ylabel('Temperature (C)')
-plt.title('Temperature Graph')
-print("The temperatures are:",T)
+plt.plot(x, T, color='red', linestyle='dashed', marker='.', markerfacecolor='blue')
+
+# Adding labels and title to the plot
+plt.xlabel('Distance (m)')
+plt.ylabel('Temperature (°C)')
+plt.title('Temperature Distribution Across the Slab')
+
+# Display the calculated temperature values
+print("The temperature values at each point are:", T)
+
+# Show the plot
+plt.show()
