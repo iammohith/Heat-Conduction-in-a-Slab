a9e7c3794cfc7d2bfe80b77c01c5ef78449b9be2
Mohith Sai Gorla <46181469+iammohith@users.noreply.github.com>
2024-10-19 15:17:23 +0530

    Update Analytical_solution_for_1D_Heat_Conduction_in_a_slab_with_heat_generation.py

--- Analytical_solution_for_1D_Heat_Conduction_in_a_slab_with_heat_generation.py
+++ Analytical_solution_for_1D_Heat_Conduction_in_a_slab_with_heat_generation.py
@@ -4,30 +4,60 @@ Created on Sun Apr 12 17:33:32 2020
 
 @author: Mohith Sai
 """
-""" Let us consider a slab with thickness of L with heat generation Qg
-and T1 & T2 be the temperatures of the both ends respectively K thermal conductivity of the material
- the temperature distributuion over a slab is given by 
- T(x) = -(Qg/2*k)*x**2 + (T2-T1)*x/L + Qg*L*x/2*k + T1 """
+
+# This program calculates the temperature distribution across a slab of thickness L, 
+# with heat generation (Qg) and known temperatures (T1 and T2) at both ends. 
+# The material has a thermal conductivity K.
+# The temperature distribution is given by the equation:
+# T(x) = -(Qg/2*K)*x**2 + (T2-T1)*x/L + (Qg*L*x)/(2*K) + T1
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-T1 = 1000 #the temperature on first end and units must be degC
-T2 = 10 #the temperature on first end and units must be degC
-L = 1 #the length of the slab units must be in meters
-Qg = 10 #heat generation units must be in W/m**3
-K = 1000 #thermal conductivity units must be in W/m*k
-n = 20 #the number of points
-x = np.linspace(0, L, n) #linearlyspaced points
-T = np.ones(n) #preallocating array of ones for temperature
-const = Qg/(2*K) #creating a const variable which refers to value of Qg/2*k
-for i in range(0, n) :
-    T[i] = -(const*x[i]**2) + ((T2-T1)*x[i])/L + const*L*x[i] + T1 #temperature distribution
+# Defining the temperatures at both ends of the slab (in degrees Celsius)
+T1 = 1000  # Temperature at one end of the slab (degC)
+T2 = 10    # Temperature at the other end of the slab (degC)
+
+# Defining the thickness of the slab (in meters)
+L = 1  # Length of the slab (m)
+
+# Heat generation rate (in W/m^3)
+Qg = 10  # Heat generation per unit volume (W/m^3)
+
+# Thermal conductivity of the slab material (in W/m*K)
+K = 1000  # Thermal conductivity (W/m*K)
+
+# Number of points along the slab to compute the temperature
+n = 20  # Number of points
+
+# Generate n linearly spaced points between 0 and L
+x = np.linspace(0, L, n)
+
+# Preallocate an array for the temperature values
+T = np.ones(n)
+
+# Constant term for Qg/(2*K), used in the temperature distribution equation
+const = Qg / (2 * K)
+
+# Calculate temperature distribution across the slab
+for i in range(n):
+    T[i] = -(const * x[i]**2) + ((T2 - T1) * x[i]) / L + (const * L * x[i]) + T1
+
+# Plotting the temperature distribution
 plt.figure(1)
-plt.plot(x,T,color='red', linestyle='dashed', marker='.', markerfacecolor='blue')
-plt.xlabel('Distacnce (m)')
-plt.ylabel('Temperature (C)')
-plt.title('Temperature Graph')
-print("The temperatures are:",T)
-"""If you want to see parabolic curve set the T1=T2 or 
-set the T1 and T2 with minimal difference with more heat generation"""
+plt.plot(x, T, color='red', linestyle='dashed', marker='.', markerfacecolor='blue')
+
+# Adding labels and title to the plot
+plt.xlabel('Distance (m)')
+plt.ylabel('Temperature (°C)')
+plt.title('Temperature Distribution Across the Slab')
+
+# Display the calculated temperature values
+print("The temperature values at each point are:", T)
+
+# Show the plot
+plt.show()
+
+# Additional Note:
+# If you want to see a parabolic curve in the temperature distribution, 
+# set T1 = T2 or choose values of T1 and T2 that are very close with higher heat generation (Qg).
